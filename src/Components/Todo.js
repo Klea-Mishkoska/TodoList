00b4d1c3a076ba9e8bd02162ed0b59cffca6b9ee
KlementinaMishkoska <102857933+KlementinaMishkoska@users.noreply.github.com>
2022-06-24 00:00:10 +0200

    Update Todo.js

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -21,7 +21,6 @@ const Todo = ({text, todo, todos, setTodos}) => {
     return (
         <div className='todo'>
             <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
-            {/*we use the {} so that we can type js `` and $ so that if the todo is completed then the style is frrom the completed class if not then we dont add anything just ''...*/}
             <button onClick={completeHandler} className='complete-btn'>
                 <i className='fas fa-check'></i>
             </button>
