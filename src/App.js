fd24060c2af92dd8d3fff59de361521c2fd51c87
KlementinaMishkoska <102857933+KlementinaMishkoska@users.noreply.github.com>
2022-06-24 00:02:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, {useState, useEffect} from "react";
-//usestate is so thaat we can import something specific from the react library
-//in this case that is usestate
 import './App.css';
 import Form from "./Components/Form";
 import TodoList from "./Components/TodoList";
@@ -8,8 +6,6 @@ import todo from "./Components/Todo";
 
 function App() {
     const [inputText, setInputText] = useState('');
-    //we have empty pair of strings here so that when we hit enter, the input stays
-    // clear and empty
     const [todos, setTodos] = useState([]);
     const [status, setStatus] = useState ('all');
     const [filteredTodos, setFilteredTodos] = useState([])
@@ -50,13 +46,8 @@ function App() {
         }
     };
 
-  //  here in the initial state we put [] because we will have array of objects here
-  //  the inputtext is the initial value, the setinputtext is a function that
-  //   allows us to change the initial value
-  //  we put the state here up, so that we can pass it down in the form and in todo, and also use it in other components,we can only pass state and props downwards, not upwards
   return (
     <div className="App">
-        {/*we say classname because class is used in js */}
         <header>
             <h1>Klea's Todo List</h1>
         </header>
@@ -67,8 +58,6 @@ function App() {
             setInputText={setInputText}
             setStatus={setStatus}
         />
-        {/*we pass it here in props, then we import it in the form component so we*/}
-        {/*can use it*/}
         <TodoList setTodos={setTodos}
                   todos={todos}
                   filteredTodos = {filteredTodos}
